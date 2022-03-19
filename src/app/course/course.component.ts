51cbcb1d114e34dfbd721fbd9a22c110babb0efb
Aakash <goplaniaakash14@gmail.com>
2022-03-19 18:18:44 +0530

    module 2 pending files

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { finalize } from 'rxjs';
 import { Course } from '../model/course';
+import { Lesson } from '../model/lesson';
 import { CoursesService } from '../services/courses.service';
 
 @Component({
@@ -8,10 +10,12 @@ import { CoursesService } from '../services/courses.service';
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css']
 })
-export class CourseComponent {
+export class CourseComponent implements OnInit {
 
-  loading = false;
+  loading: boolean = false;
+  lastPageLoaded: number = 0;
   course: Course;
+  lessons!: Lesson[];
   displayedColumns = ['seqNo', 'description', 'duration'];
 
   constructor(
@@ -21,4 +25,21 @@ export class CourseComponent {
     this.course = this.route.snapshot.data['course'];
   }
 
+  ngOnInit(): void {
+    this.loading = true;
+    this.fetchDataFromLessons();
+  }
+
+  loadMoreData() {
+    this.lastPageLoaded++;
+    this.fetchDataFromLessons('asc');
+  }
+
+  private fetchDataFromLessons(sortingOrder: any = 'asc') {
+    this.coursesService.findLessons(this.course.id, sortingOrder, this.lastPageLoaded).pipe(
+      finalize(() => this.loading = false)
+    ).subscribe((lessons) => {
+      this.lessons = this.lessons ? this.lessons.concat(lessons) : lessons;
+    });
+  }
 }
