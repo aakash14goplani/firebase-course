9ea630b4ce7b85822bb8194c4ded9b93f2fbbde6
Aakash <goplaniaakash14@gmail.com>
2022-03-24 12:47:10 +0530

    updated prod env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,17 @@
+import secretKey from './secrets';
+
 export const environment = {
   production: true,
-  useEmulators: false
+  useEmulators: false,
+  firebase: {
+    apiKey: secretKey.firebase,
+    authDomain: secretKey.firebaseAuthDomain,
+    projectId: secretKey.firebaseProjectId,
+    storageBucket: secretKey.firebaseStorageBucket,
+    messagingSenderId: secretKey.firebaseMessagingSenderId,
+    appId: secretKey.firebaseAppId
+  },
+  api: {
+    createUser: 'http://localhost:5001/angular-fire-a31ae/us-central1/createUser'
+  }
 };
